eda438dd8eed157de0fbbb626242dff468395f29
ellisandrews <ellisandrews1@gmail.com>
2020-02-28 09:27:21 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,64 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    
+    results = DB[:conn].execute("SELECT * FROM students ORDER BY id;")
+
+    results.map { |row| self.new_from_db(row) }
+
   end
 
+  # TODO: Refactor this class to use this helper method! More DRY. See `self.first_X_students_in_grade_10`
+  def self.students_by_query(query, *params)
+    results = DB[:conn].execute(query, *params)
+    results.map { |row| self.new_from_db(row) }
+  end
+
+  def self.first_X_students_in_grade_10(num_students)
+    query = "SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT ?;"
+    self.students_by_query(query, num_students)
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)[0]
+  end
+
+
   def self.find_by_name(name)
+    row = DB[:conn].execute("SELECT * FROM students WHERE name = ? LIMIT 1;", name).first
+    self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_X(grade)
     # find the student in the database given a name
     # return a new instance of the Student class
+    results = DB[:conn].execute("SELECT * FROM students WHERE grade = ?;", grade)
+    results.map { |row| self.new_from_db(row) }
   end
   
+  def self.all_students_in_grade_9
+    self.all_students_in_grade_X(9)
+  end
+
+  def self.students_below_grade(grade)
+    results = DB[:conn].execute("SELECT * FROM students WHERE grade < ?;", grade)
+    results.map { |row| self.new_from_db(row) }
+  end
+
+  def self.students_below_12th_grade
+    self.students_below_grade(12)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
